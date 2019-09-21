db60346ef815c9219e7c3d86cf93775e4488d7e1
benjchand <benchand@gmail.com>
2019-09-20 21:24:09 -0700

    Finished looking at this for the sprint.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -5,8 +5,7 @@ as well as its next node in the List."""
 class ListNode:
     def __init__(self, value, prev=None, next=None):
         self.value = value
-        self.prev = prev
-        self.next = next
+        self.next = 
 
     """Wrap the given value in a ListNode and insert it
   after this node. Note that this node could already
@@ -59,7 +58,6 @@ class DoublyLinkedList:
             self.tail = new_node
 
         else:
-            self.head.prev = new_node
             new_node.next = self.head
             self.head = new_node
 
@@ -79,8 +77,73 @@ class DoublyLinkedList:
             self.head.prev = None
             return current_head.value
 
+    def insert(self, value, before_node):
+        new_node = ListNode(value, prev=None, next=None)
+        current = self.head
+        while current.next is not None:
+# Check to see if node is before_node.
+# If it is, then I know where I need to insert.
+            if current is before_node:
+                # Assign new_node's next value to be the current next.
+                new_node.next = current.next
+                # Assign current's next value to be the new_node.
+                current.next = new_node
+                break
+# If it is NOT, then I reassign current, 
+#   so that I can move down the list and check the next value
+            else:
+                current = current.next
+# If I don't find the before_node, print an error
+        print("Ya done messed up, man.  That before_node is wack.")
+        return
+
+
+    def reassign(self, node):
+# Keep track of where I am in the list
+#   (probably do this with a current_node variable)
+# Start at the head of the list
+#   (current_node is going to be self. head)
+        current_node = self.head
+
+# Create flag that will be false until I FIND NODE!
+        foundIt = False
+
+# Iterate over the entire list
+#   (Don't know how long it is, so I think it will be a while loop)
+# While Loop will be while current_node.next is not None
+        while current_node.next is not None:
+
+    # LOOK FORWARD!  Check the current node's neighbor.  It's NEXT, 
+    # If the current_node.next is the one I am looking for:
+            if current_node.next is node:
+        # Reassign the current_node.next to be current_node.next.next
+                current_node.next = current_node.next.next
+        # set flag to be true.
+                foundIt = True
+                break
+    # Else, move on to the next node
+    #   (current_node = current_node.next)
+            else: 
+                current_node = current_node.next
+
+# If flag is true, then node was found and I need to put it at the head.
+# Else print a statement that the node wasn't found in the list.
+        if foundIt is True:
+            node.next = self.head
+            self.head = node
+        else:
+            print("That node could not be found.  Initate failsafe. Destroy evidence.")
+
+
+
+
+
+
     def add_to_tail(self, value):
         new_node = ListNode(value, prev=None, next=None)
+        
+
+
         self.length += 1
         if not self.tail:
             self.head = new_node
@@ -146,3 +209,9 @@ class DoublyLinkedList:
             current = current.next
 
         return max_value
+
+
+
+
+
+
--- doubly_linked_list/text_buffer.py
+++ doubly_linked_list/text_buffer.py
@@ -9,7 +9,7 @@ class TextBuffer:
         # check if an init string is provided
         # if so, put the contents of the init string in self.contents
         if init:
-            self.contents = DoublyLinkedList.__init__
+            self.append(init)
 
     def __str__(self):
         # needs to return a string to print
@@ -21,19 +21,27 @@ class TextBuffer:
         return s
 
     def append(self, string_to_add):
-        pass
+        for c in string_to_add:
+            self.contents.add_to_tail(c)
 
     def prepend(self, string_to_add):
         # reverse the incoming string to maintain correct
         # order when adding to the front of the text buffer
-        pass
+        for c in string_to_add[::-1]:
+            self.contents.add_to_head(c)
 
     def delete_front(self, chars_to_remove):
-        pass
+        while chars_to_remove:
+            self.contents.remove_from_head()
+            chars_to_remove -= 1
 
     def delete_back(self, chars_to_remove):
-        pass
+        while chars_to_remove:
+            self.contents.remove_from_tail()
+            chars_to_remove -= 1
 
+    # for i in range(chars_to_remove):
+    #     self.contents.remove_from_tail()
     """
     Join other_buffer to self
     The input buffer gets concatenated to the end of this buffer 
@@ -45,15 +53,23 @@ class TextBuffer:
         # we might want to check that other_buffer is indeed a text buffer
         # set self list tail's next node to be the head of the other buffer
 
-        # set other_buffer head's prev node to be the tail of this buffer
+        if isinstance(other_buffer, TextBuffer):
+            self.contents.tail.next = other_buffer.contents.head
+            other_buffer.contents.head.prev = self.contents.tail
+            self.contents.tail = other_buffer.contents.tail
+            self.contents.length += other_buffer.contents.length
+        else:
+            print("ERROR: arg is not a text buffer")
+            return
+            # set other_buffer head's prev node to be the tail of this buffer
 
-        pass
+            # if we get fed a string instead of a text buffer instance,
+            # initialize a new text buffer with this string and then
+            # call the join method
 
-    # if we get fed a string instead of a text buffer instance,
-    # initialize a new text buffer with this string and then
-    # call the join method
     def join_string(self, string_to_join):
-        pass
+        new_buffer = TextBuffer(string_to_join)
+        self.join(new_buffer)
 
 
 if __name__ == '__main__':
